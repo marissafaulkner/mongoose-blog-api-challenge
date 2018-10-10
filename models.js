b062a2cff017901d911517cab79d4bf1896142e8
Marissa Faulkner <marissagrace808@gmail.com>
2018-10-10 13:58:55 -0700

    completed models, get requests in server

--- models.js
+++ models.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const mongoose = require("mongoose");
+
+
+//schema to represent a blogpost
+const postSchema = mongoose.Schema({
+	title: { type: String, require: true },
+	content: { type: String, require: true },
+	author: {
+		firstName: { type: String, require: true },
+		lastName: { type: String, require: true }
+	}
+});
+
+
+//virtual for the authors full name
+postSchema.virtual("authorFullName").get(function() {
+	return `${this.author.firstName} ${this.author.lastName}`
+});
+
+//serializing the data
+postSchema.methods.serialize = function() {
+	return {
+		id: this._id,
+		title: this.title,
+		content: this.content,
+		author: this.authorFullName
+	};
+};
+
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = { Post };
--- server.js
+++ server.js
@@ -12,3 +12,32 @@ const { Post } = require("./models");
 
 const app = express();
 app.use(express.json());
+
+
+//GET requests to /posts returns all posts
+app.get("/posts", (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.json({
+        posts: posts.map(post => post.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
+
+
+//request by ID
+app.get("/posts/:id", (req, res) => {
+  Post
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(post => res.json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
